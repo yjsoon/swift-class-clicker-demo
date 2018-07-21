4b2e3ab07503e6f8578bd27663697a3920185540
YJ Soon <yjsoon@tinkertanker.com>
2018-07-21 09:20:33 +0800

    Done initial setup for ClickerViewController

--- Clicker Version 9/ClickerViewController.swift
+++ Clicker Version 9/ClickerViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ClickerViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,8 @@ class ClickerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func tapped(_ sender: Any) {
+    }
+    
 }
 
