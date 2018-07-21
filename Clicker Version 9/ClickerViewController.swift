6b705a9e1da4cc918b19401a0756d9b85312cc4d
YJ Soon <yjsoon@tinkertanker.com>
2018-07-21 09:21:52 +0800

    Added logic to ClickerViewController

--- Clicker Version 9/ClickerViewController.swift
+++ Clicker Version 9/ClickerViewController.swift
@@ -15,15 +15,14 @@ class ClickerViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func tapped(_ sender: Any) {
+        counter += 1
+        label.text = String(counter)
+        if counter == 30 {
+            print("You're done!")
+        }
     }
     
 }
